be9f364f35854b46611c68d868c3b82315f1269e
Vihaan Dharia <vihaandharia04@gmail.com>
2024-07-25 17:14:25 +0530

    third

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,45 @@
-import './globals.css'
 import { Inter as FontSans } from "next/font/google"
-import { Metadata } from 'next'
+
 import { cn } from "@/lib/utils"
-import {ClerkProvider} from '@clerk/nextjs'
-import {dark} from '@clerk/themes'
-import Provider from './Provider'
+import './globals.css'
+import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-export const metdata: Metadata={
+export const metadata: Metadata = {
   title: 'LiveDocs',
-  description: 'LiveDocs is a documentation tool for developers',
+  description: 'Your go-to collaborative editor',
 }
 
-export default function RootLayout({ children }: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider appearance={{baseTheme:dark,variables : {colorPrimary: "#3371FF", fontSize: '16px'}}}>
-    <html lang="en" suppressHydrationWarning>
-     
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-      <Provider>
-      {children}
-      </Provider>
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { 
+          colorPrimary: "#3371FF" ,
+          fontSize: '16px'
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Provider>
+            {children}
+          </Provider>
+        </body>
+      </html>
     </ClerkProvider>
   )
 }
